ab0a49548c5126633178e427d0b3b1a56297e7de
vansaikr <116336719+vansaikr@users.noreply.github.com>
2023-01-09 10:47:10 +0530

    Update streamlit_app.py
    changed parents to moms

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("My parent's healty new diner")
+streamlit.title("My mom's healty new diner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text("🥗 Kale, Spinach and Rocket Smoothie")
